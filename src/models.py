e0b5860b22515ff8fc8beed6c5f4d95de9d020b3
Avilarrealr <149917560+Avilarrealr@users.noreply.github.com>
2025-11-02 18:32:06 +0000

    creando diagrama UML y modelos con sqlalchemy

--- src/models.py
+++ src/models.py
@@ -1,15 +1,65 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import List
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
+    __tablename__ = "user"
+
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
+
+    posts: Mapped[List["Post"]] = relationship(back_populates="user")
+    comments: Mapped[List["Comment"]] = relationship(back_populates="user")
+    favorites: Mapped[List["Favorite"]] = relationship(back_populates="user")
+
+
+class Post(db.Model):
+    __tablename__ = "post"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    caption: Mapped[str] = mapped_column(String(320))
+
+    # Relationships childs
+    comments: Mapped[List["Comment"]] = relationship(back_populates="post")
+    favorites: Mapped[List["Favorite"]] = relationship(back_populates="post")
+
+    # Relationships parents
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    user: Mapped["User"] = relationship(back_populates="posts")
+
+
+class Comment(db.Model):
+    __tablename__ = "comment"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    comment_text: Mapped[str] = mapped_column(String(320), nullable=False)
+
+    # Relationships parents
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    user: Mapped["User"] = relationship(back_populates="comments")
+
+    post_id: Mapped[int] = mapped_column(ForeignKey("post.id"))
+    post: Mapped["Post"] = relationship(back_populates="comments")
+
+
+class Favorite(db.Model):
+    __tablename__ = "favorite"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
 
+    # Relationships parents
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    user: Mapped["User"] = relationship(back_populates="favorites")
+
+    post_id: Mapped[int] = mapped_column(ForeignKey("post.id"))
+    post: Mapped["Post"] = relationship(back_populates="favorites")
 
     def serialize(self):
         return {
